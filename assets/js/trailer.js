fe3e81e455a247a2a61710c3dd81fe6062aa0942
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-07-15 14:20:22 +0200

    Maj code + visible

--- assets/js/trailer.js
+++ assets/js/trailer.js
@@ -7,27 +7,37 @@ fetch(`https://api.themoviedb.org/3/movie/${filmId}/videos?language=fr-FR`, opti
             noFilm = false
             film = data
             console.log(film)
+
             if (film.results.length > 0) { //Avant de filtrer, on vérifie qu'il y a des trailers
                 film = film.results.filter(video => video.type == "Trailer") //Filtre pour ne garder que les trailers
                 console.log(film)
                 filmTrier = []
+
                 if (film.length > 0) { //Avant de trier, on vérifie qu'il y a des trailers
+
                     for (let f = 0; f < film.length; f++) { //Push en premier les VF
+
                         if (film[f].name.toUpperCase().includes("VF")) {
                             filmTrier.push(film[f])
                             console.log(filmTrier)
                         }
                     }
+
                     for (let v = 0; v < film.length; v++) { //Push en second les VOST
+
                         if (film[v].name.toUpperCase().includes("VOST")) {
                             filmTrier.push(film[v])
                             console.log(filmTrier)
                         }
                     }
+
                     filmTrier.push(film[0]) //Ajoute le premier trailer si pas de VF ou VOST
                     console.log(filmTrier)
+
                 }
+                
             }
+
             if (filmTrier.length < 1 || film.length < 1) {
                 noFilm = true
                 film = `https://www.youtube.com/embed/1` //Si pas de trailer, on met une vidéo par défaut
